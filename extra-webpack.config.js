907ca2bba67f4d77cd4cfc6dac11fba799d047c6
kevin <github@gameknave.com>
2020-08-21 17:44:10 -0700

    Tracking some files

---
+++ extra-webpack.config.js
@@ -0,0 +1,12 @@
+const merge = require('webpack-merge');
+
+module.exports = function (angularConfig) {
+  console.log("custom webpackin");
+    return merge(angularConfig, {
+        module: {
+            rules: [
+              { test: /\.haml$/, loader: 'haml-haml' }
+            ]
+          }
+        })
+    }
